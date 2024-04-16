cfab3d95beab2d9e408d6373291ab56a4d6c26b5
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2024-04-16 16:10:28 +0200

    save progress on create headings part 2

--- src/modules/index.js
+++ src/modules/index.js
@@ -27,37 +27,40 @@
 
 function createHeadings(array) {
   let newElement = "";
-  const arrayHeadings = [];
+
+  // Select each element from the Markdown array
+  // Boucle sur parameter: array
   array.forEach((line) => {
-    if (line.trim().startsWith("#")) {
-      let level = 0;
-      // Check if number of # is between 0 and 6
-      if(level < 0 && level <= 6) {
-        while(line[level] === "#") {
-          level += 1;
-        }
-        arrayHeadings.push({ level: level, value: line.slice(level) })
+    if (line.startsWith("#")) {
+      let level = 0; // Attribute 1 to heading level
+      // Check if level of # is between 0 and 6
+      while (line[level] === "#") {
+        level += 1;
+      }
+      if (level > 0 && level <= 6) {
+        // Ex. return "<h1>" + markdown + "</h1>";
+        let textContent = line.slice(level);
+        newElement += `<h${level}>${textContent}</h${level}>`;
       } else {
-        console.error("Bad increment for heading");
+        newElement = line;
       }
     }
   });
-  for (const heading of arrayHeadings) {
-    newElement += `<h${heading.level}>${heading.value}</h${heading.level}>`;
-  }
   return newElement;
   // return `<h${level}> ${value} </h${level}>`; // Ex. return "<h1>" + markdown + "</h1>";
 }
 
 const markdownToHtml = (markdown) => {
   let html = "";
-  let arrayMarkdown = markdown.split(" ");
+  let arrayMarkdown = markdown.split("\n");
+
   html += createHeadings(arrayMarkdown);
   // return arrayMarkdown.join(""); // Retourner le tableau sous format de string
+
   return html;
 };
 
-const text = "#####tatatatata";
+const text = "##tatatatata totototo";
 
 console.log(markdownToHtml("Test"));
 console.log(markdownToHtml(text));

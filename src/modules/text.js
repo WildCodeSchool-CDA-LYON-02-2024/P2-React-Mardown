28a002f8b26bb728d64717662e351e39dcbc9166
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2024-04-24 23:02:00 +0200

    add function list

--- src/modules/text.js
+++ src/modules/text.js
@@ -1,33 +1,131 @@
 /**
  * Create headings of website with markdown syntax
  */
-export function createHeadings(array) {
+export function createHeadings(lines) {
   let newElement = "";
-  let level = 1; // Attribute 1 to heading level
 
-  // Select each element from the Markdown array
-  // Boucle sur parameter: array
-  array.forEach((line) => {
-    // Count how many # in the line
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    let level = 0; // Réinitialiser le niveau de titre pour chaque ligne
+
+    // Compter le nombre de signes '#' au début de la ligne pour déterminer le niveau de titre
     while (line[level] === "#") {
-      level += 1;
+      level++;
     }
-    // If starts with '#' & Check if level of # is between 0 and 6
+
     if (level > 0 && level <= 6) {
-      // Ex. return "<h1>" + markdown + "</h1>";
-      let textContent = line.slice(level).trim();
-      newElement += `<h${level}>${textContent}</h${level}>`;
-    } else {
-      newElement = line; // return line as is
+      // Exclure les signes '#' et les espaces pour obtenir le texte du titre
+      const textContent = line.slice(level).trim();
+      // Créer la balise HTML en fonction du niveau de titre
+      newElement += `<h${level}>${textContent}</h${level}>\n`;
     }
-  });
+  }
+
   return newElement;
 }
 
-export function createParagraph(array) {
+export function createParagraph(lines) {
   let newElement = "";
-  array.forEach((line) => {
-    newElement += `<p>${ line }</p>`;
-  });
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    newElement += `<p>${line}</p>\n`;
+  }
   return newElement;
 }
+
+export function createUnorderedList(lines) {
+  let newElement = "<ul>";
+
+  let listLines = []; // pour stocker les lignes de la liste en cours
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim();
+
+    if (
+      line.startsWith("- ") ||
+      line.startsWith("* ") ||
+      line.startsWith("+ ")
+    ) {
+      listLines.push(line);
+
+      // Vérifie si la prochaine ligne n'est pas une liste ou si c'est la dernière ligne
+      if (i === lines.length - 1 || lines[i + 1].charAt(0) !== line.charAt(0)) {
+        // Crée la liste à partir des lignes accumulées
+        newElement += createListItems(listLines);
+        listLines = [];
+      }
+    }
+  }
+
+  newElement += "\n</ul>\n"; // Ajoute un retour à la ligne après la fermeture de la balise <ul>
+  return newElement;
+}
+
+export function createOrderedList(lines) {
+  let newElement = "<ol>";
+
+  let listLines = [];
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim();
+
+    // Vérifiez si la ligne commence par un chiffre suivi d'un point
+    if (/^\d+\.\s/.test(line)) {
+      if (listLines.length > 0) {
+        // S'il y a des lignes dans listLines, créez la liste ordonnée avec les lignes accumulées
+        newElement += createListItems(listLines);
+        listLines = [];
+      }
+      // Ajoutez la ligne courante à listLines
+      listLines.push(line);
+    } else {
+      // Si la ligne ne commence pas par un chiffre suivi d'un point et d'un espace,
+      // ajoutez simplement la ligne telle quelle à la balise <ol>
+      newElement += `<li>${line}</li>`;
+    }
+
+    // Vérifiez si la prochaine ligne n'est pas une liste ordonnée ou si c'est la dernière ligne
+    if (i === lines.length - 1 || !/^\d+\.\s/.test(lines[i + 1])) {
+      if (listLines.length > 0) {
+        newElement += createListItems(listLines);
+        listLines = [];
+      }
+    }
+  }
+
+  newElement += "\n</ol>\n";
+  return newElement;
+}
+
+function createListItems(lines) {
+  let listItems = "";
+
+  for (let line of lines) {
+    let listItemContent = line.trim();
+    if (/^\d+\./.test(line)) {
+      // Pour les listes ordonnées, retirez le chiffre et le point
+      listItemContent = line.replace(/^\d+\./, "").trim();
+    } else if (/^[-*+]\s/.test(line)) {
+      // Pour les listes non ordonnées, retirez le tiret, l'astérisque ou le plus
+      listItemContent = line.replace(/^[-*]\s/, "").trim();
+    }
+    // Ajoutez le contenu de l'élément de liste avec un retour à la ligne et une indentation
+    if (listItems === "") {
+      // Si c'est le premier élément de la liste, ajoutez un retour à la ligne avant
+      listItems += `\n  <li>${listItemContent}</li>`;
+    } else {
+      // Sinon, ajoutez normalement
+      listItems += `\n  <li>${listItemContent}</li>`;
+    }
+  }
+
+  return listItems;
+}
+
+// const text =
+//   "## **Fournir une documentation**\n\n **claire et des exemples de code pour\nesfiuhsiufhsdigfyqgfiyqgfiygiydgqfiqgdiyqfgqiudfgiqgdfiqudgfiudqg\n\n\n\n #### de votre bibliothèque.\n\nFournir un site d'exemple et un StoryBook pourudyugqsduygqsuyfgsdyufguydfg\ndfsdfdsdfsfsfsdsf \n\nprésenter les fonctionnalités de la bibliothèque.";
+// console.log(createHeadings(text));
+// console.log("------------------------------");
+// console.log(createParagraph(text));
+//  console.log(createList(list));
--- src/services/markdownToHtml.js
+++ src/services/markdownToHtml.js
@@ -1,4 +1,10 @@
-import { createHeadings, createParagraph } from "../modules/text.js";
+import {
+  createHeadings,
+  createParagraph,
+  createUnorderedList,
+  createOrderedList,
+} from "../modules/text.js";
+import { getLine } from "./getLine.js";
 // import { createBold, createItalic } from "../modules/inline.js";
 
 /**
@@ -23,69 +29,90 @@ import { createHeadings, createParagraph } from "../modules/text.js";
  * @returns {*[]}
  */
 
-export const runnerMarkdown = markdownContent => {
-    let arrayElements = [];
-    let element = {
-        value: ''
-    };
-    let i = 0;
-    let j = 0;
-    if(!new RegExp(/\n/).test(markdownContent)) {
-        while (i < markdownContent.length && new RegExp(/[*]|#|[0-9]|-|\s|[A-Za-z]/).test(markdownContent[i])) {
-            if(new RegExp(/[*]|#|-|[0-9]/).test(markdownContent[i])) {
-                j += 1;
-                switch (markdownContent[i]) {
-                    case '-':
-                        element.type = 'unorderedList';
-                        element.start = i + 2;
-                        break;
-                    case '#':
-                        element.type = 'title';
-                        element.start = i + 2;
-                        break;
-                    default:
-                        break;
-                }
-            } else {
-                if(new RegExp(/^[A-Za-z\s]/).test(markdownContent[i]) && i >= element.start) {
-                    element.value += i !== 0 ? markdownContent[i] : '';
-                    element.value.trimStart();
-                    element.end = j > i ? j : markdownContent.length;
-                }
-            }
-            i+= 1;
-        }
-        arrayElements.push(element);
-        for (const arrayElementValue of arrayElements) {
-            arrayElementValue.value.trimStart();
-        }
-        return arrayElements;
-    } else {
-        /** Cas saut de lignes
-         * HERE...
-         */
-    }
-};
+// export const runnerMarkdown = markdownContent => {
+//     let arrayElements = [];
+//     let element = {
+//         value: ''
+//     };
+//     let i = 0;
+//     let j = 0;
+//     if(!new RegExp(/\n/).test(markdownContent)) {
+//         while (i < markdownContent.length && new RegExp(/[*]|#|[0-9]|-|\s|[A-Za-z]/).test(markdownContent[i])) {
+//             if(new RegExp(/[*]|#|-|[0-9]/).test(markdownContent[i])) {
+//                 j += 1;
+//                 switch (markdownContent[i]) {
+//                     case '-':
+//                         element.type = 'unorderedList';
+//                         element.start = i + 2;
+//                         break;
+//                     case '#':
+//                         element.type = 'title';
+//                         element.start = i + 2;
+//                         break;
+//                     default:
+//                         break;
+//                 }
+//             } else {
+//                 if(new RegExp(/^[A-Za-z\s]/).test(markdownContent[i]) && i >= element.start) {
+//                     element.value += i !== 0 ? markdownContent[i] : '';
+//                     element.value.trimStart();
+//                     element.end = j > i ? j : markdownContent.length;
+//                 }
+//             }
+//             i+= 1;
+//         }
+//         arrayElements.push(element);
+//         for (const arrayElementValue of arrayElements) {
+//             arrayElementValue.value.trimStart();
+//         }
+//         return arrayElements;
+//     } else {
+//         /** Cas saut de lignes
+//          * HERE...
+//          */
+//     }
+// };
 
 const markdownToHtml = (markdown) => {
   let html = "";
-  let arrayMarkdown = markdown.split("\n");
-  switch (markdown.charAt(0)) {
-    case '#':
-      html += createHeadings(arrayMarkdown)
-    break;
-    default:
-      html += createParagraph(arrayMarkdown);
-    break;
+  const lines = getLine(markdown);
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    switch (line.charAt(0)) {
+      case "#":
+        html += createHeadings([line]);
+        break;
+      case "*":
+      case "-":
+        html += createUnorderedList(lines);
+        // Met à jour indice pour sauter les lignes déjà traitées
+        i += lines.length - 1;
+        break;
+      default:
+        if (/^\d+\.\s/.test(line)) {
+          html += createOrderedList(lines);
+          i += lines.length - 1;
+        } else {
+          html += createParagraph([line]);
+        }
+        break;
+    }
   }
-  // return arrayMarkdown.join(""); // Retourner le tableau sous format de string
+
   return html;
 };
 
-const titre = "######tatatatata totototo";
+const titre = "###### tatatatata totototo";
 const paragraphe = "tatatatata# totototo";
+const list = "- hksdgksdgfkhsdgfksdgfk\n- kffkdjglfdgjdfl\n- hkdsghdsgfhk";
+const ordredlist =
+  "1. hksdgksdgfkhsdgfksdgfk\n2. kffkdjglfdgjdfl\n3. hkdsghdsgfhk";
+  
 
 console.log(markdownToHtml(titre));
 console.log(markdownToHtml(paragraphe));
+console.log(markdownToHtml(list));
+console.log(markdownToHtml(ordredlist));
 
 export default markdownToHtml;

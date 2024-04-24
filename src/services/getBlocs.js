63f68794cec0063d93d1a838337709df337d806b
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2024-04-24 10:58:14 +0200

    save progress on getLine function

---
+++ src/services/getBlocs.js
@@ -0,0 +1,16 @@
+ export function getBlocs(str) {
+  const blocs = [];
+  let bloc = str.split("\n\n");
+
+  for (let i = 0; i < bloc.length; i++) {
+    if (bloc[i].trim() !== "") {
+      blocs.push(bloc[i].trim());
+    }
+  }
+  return blocs;
+}
+
+const text =
+  "Fournir une documentation\n\n claire et des exemples de code pour\n esfiuhsiufhsdigfyqgfiyqgfiygiydgqfiqgdiyqfgqiudfgiqgdfiqudgfiudqg\n\n\n\n de votre bibliothèque.\n\nFournir un site d'exemple et un StoryBook pour \n\nprésenter les fonctionnalités de la bibliothèque.";
+
+console.log(getBlocs(text));
---
+++ src/services/getLine.js
@@ -0,0 +1,14 @@
+import { getBlocs } from "./getBlocs.js";
+function getLine(bloc) {
+ let blocs = getBlocs();
+  let line = blocs.split("\n");
+  console.log(blocs);
+  for (let i = 0; i < line.length; i++) {
+    if (line[i].trim() !== "") {
+      line.push(line[i].trim());
+    }
+  }
+  return bloc;
+}
+
+console.log(getLine());
